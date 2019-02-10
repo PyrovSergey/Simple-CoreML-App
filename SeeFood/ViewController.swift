aa296d0fbd076a4c79a6204dbf2ff564dff8bed5
serge <pyrov.sergey@gmail.com>
2019-02-10 20:31:20 +0300

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Sergey on 10/02/2019.
+//  Copyright © 2019 Sergey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
